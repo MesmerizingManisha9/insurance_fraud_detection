9d9f8bdf1d82b7bde71a67219f824324b12417f6
manisha_sharma <manishaah479@gmail.com>
2023-08-21 19:29:33 +0530

    prediction

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,8 @@ import pandas as pd
 import dill
 import pickle
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV 
+from src import logger
 
 from src.exception import CustomException
 
@@ -22,15 +23,22 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train,X_test, y_test, y_train  ,models):
+def evaluate_models(X_train,X_test, y_test, y_train  ,models, params):
     try:
         
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
 
-            model.fit(X_train, y_train)  # Train model
+            gs = GridSearchCV(model,para,cv=3 )
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
 
@@ -48,9 +56,12 @@ def evaluate_models(X_train,X_test, y_test, y_train  ,models):
         raise CustomException(e, sys)
     
 def load_object(file_path):
+
     try:
         with open(file_path, "rb") as file_obj:
             return pickle.load(file_obj)
 
     except Exception as e:
+        logger.logging.info(e) # it will print exception
         raise CustomException(e, sys)
+    
